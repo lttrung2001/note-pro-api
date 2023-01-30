6974c98c8bb7da908e8d5a8c0e1ba88144e93082
Le Thanh Trung <lt.trung2001@gmail.com>
2023-01-30 21:36:37 +0700

    add HttpStatusCode and update app.js

--- src/app.js
+++ src/app.js
@@ -1,13 +1,5 @@
-/**
-This code creates an Express.js web server and uses the "express-fileupload" middleware
-to handle file uploads. The server is set to listen on either the port specified by the
-"process.env.PORT" environment variable or port 3000. It also sets limits for file size and
-number of files to 2MB and 10 respectively. The other middleware used are express.json() and
-express.urlencoded() for handling json and urlencoded data respectively.
-*/
 const express = require('express')
 const fileUpload = require('express-fileupload')
-const { credential } = require('firebase-admin')
 
 const app = express()
 
@@ -23,30 +15,8 @@ app.use(fileUpload({
    }
 }))
 
-app.get('/test/register', (req, res) => {
-   const firebaseApp = require('./configs/firebaseConfig')
-   const firebaseAuth = require('firebase/auth')
-   const auth = firebaseAuth.getAuth(firebaseApp)
-   firebaseAuth.createUserWithEmailAndPassword(auth, 'lt.trung2001@gmail.com', '123456').then((credential) => {
-      // See the UserRecord reference doc for the contents of userRecord.
-      console.log('Successfully created new user:', credential.user);
-      firebaseAuth.sendEmailVerification(credential.user)
-      res.status(200).json({ message: 'Successfully created new user' })
-   }).catch(function (error) {
-      console.log('Error creating new user:', error);
-      res.status(500).json({ message: 'Error creating new user' })
-   })
 })
 
-app.get('/test/login', (req, res) => {
-   const firebaseApp = require('./configs/firebaseConfig')
-   const firebaseAuth = require('firebase/auth')
-   const auth = firebaseAuth.getAuth(firebaseApp)
-   firebaseAuth.signInWithEmailAndPassword(auth, 'lt.trung2001@gmail.com', '123456').then((credential) => {
-      res.status(200).json({data: credential.user})
-   }).catch((error) => {
-      res.status(200).json({data: error.message})
-   })
 })
 
 app.listen(process.env.PORT || 3000, () => {
--- src/configs/firestoreConfig.js
+++ src/configs/firestoreConfig.js
@@ -1,10 +1,10 @@
 const admin = require("firebase-admin");
 const serviceAccount = require("../../note-pro-91c72-firebase-adminsdk-iazk3-2346ed8221.json");
 
-const app = admin.initializeApp({
+admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
 });
 
 const firestore = admin.firestore();
-const auth = admin.auth()
-module.exports = {firestore, auth}
+const adminAuth = admin.auth()
+module.exports = {firestore, adminAuth}
