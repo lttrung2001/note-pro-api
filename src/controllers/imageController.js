27c711cd09e2ed62e4fd52e9148e81f93fe6494b
Le Thanh Trung <lt.trung2001@gmail.com>
2023-02-08 14:38:22 +0700

    edit code in service and controller

--- src/controllers/imageController.js
+++ src/controllers/imageController.js
@@ -1,38 +1,10 @@
-import { StatusCodes } from 'http-status-codes'
-import getImagesService from '../services/imageServices/getImages'
+import getImagesService from "../services/imageServices/getImages";
 const getImages = async (req, res) => {
-    const { noteId, pageIndex, limit } = req.query
-    if (!(noteId && pageIndex && limit)) {
-        res.status(StatusCodes.BAD_REQUEST).json({
-            message: 'All input required.',
-            data: null
-        })
-    } else if (pageIndex < 0) {
-        res.status(StatusCodes.BAD_REQUEST).json({
-            message: 'Page index must be equal or greater than 0.',
-            data: null
-        })
-    } else if (limit < 0) {
-        res.status(StatusCodes.BAD_REQUEST).json({
-            message: 'Limit must be greater than 0.',
-            data: null
-        })
-    }
-    try {
-        const data = await getImagesService(noteId, pageIndex, limit)
-        res.status(StatusCodes.OK).json({
-            message: 'Get images successfully.',
-            data: data
-        })
-    } catch (error) {
-        console.log(error.message)
-        res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
-            message: 'Get images failed.',
-            data: null
-        })
-    }
-}
+  const { noteId, pageIndex, limit } = req.query;
+  const serviceResult = await getImagesService(noteId, pageIndex, limit);
+  res.status(serviceResult.code).json(serviceResult.body());
+};
 
 module.exports = {
-    getImages
-}
+  getImages,
+};
