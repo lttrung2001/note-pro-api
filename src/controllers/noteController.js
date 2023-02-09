502bdd0a23b83e0a61768a9a7f774fd1e1ca4725
Le Thanh Trung <lt.trung2001@gmail.com>
2023-02-09 09:52:14 +0700

    Update noteController.js

--- src/controllers/noteController.js
+++ src/controllers/noteController.js
@@ -7,7 +7,7 @@ import deleteNoteService from '../services/noteServices/deleteNote'
 const addNote = async (req, res) => {
     try {
         const note = new Note(null, req.body.title, req.body.content, Date.now())
-        const member = new Member(uid, 'owner', req.body.isPin)
+        const member = new Member(req.user.uid, 'owner', req.body.isPin)
         const files = req.files
         const addNoteServiceResult = await addNoteService(note, member, files)
         res.status(addNoteServiceResult.code).json(addNoteServiceResult.body())
