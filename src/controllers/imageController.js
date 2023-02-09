89ff9418191ea881a95159db3ab5db75178af95d
Le Thanh Trung <lt.trung2001@gmail.com>
2023-02-09 17:27:06 +0700

    code service, controller, router

--- src/controllers/imageController.js
+++ src/controllers/imageController.js
@@ -1,5 +1,6 @@
 import { StatusCodes } from "http-status-codes";
 import getImagesService from "../services/imageServices/getImages";
+import deleteImageService from "../services/imageServices/deleteImage";
 const getImages = async (req, res) => {
   const { noteId, pageIndex, limit } = req.query;
   try {
@@ -12,6 +13,23 @@ const getImages = async (req, res) => {
   }
 };
 
+const deleteImage = async (req, res) => {
+  const { noteId, imageId } = req.query
+  const uid = req.user.uid
+  try {
+    const deleteImageServiceResult = await deleteImageService(noteId, imageId, uid)
+    res.status(deleteImageServiceResult.code).json({
+      message: deleteImageServiceResult.message,
+      data: deleteImageServiceResult.data
+    })
+  } catch (error) {
+    res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+      message: error.message
+    })
+  }
+}
+
 module.exports = {
   getImages,
+  deleteImage
 };
