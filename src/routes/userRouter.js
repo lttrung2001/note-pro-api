a28cf9d9ce8db2f7abf20d3cafd47b457f892dbc
Duke <agueroduc01@gmail.com>
2023-02-11 13:55:58 +0700

    changePassword

--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -1,10 +1,19 @@
 import { Router } from "express";
+import { checkAccessToken } from "../middleware/JWT";
 import userController from "../controllers/userController";
 
 const userRouter = () => {
   const router = Router();
   router.post("/register", userController.registerUser);
   router.post("/login", userController.loginUser);
+  router.post(
+    "/changePassword",
+    checkAccessToken,
+    userController.changePassword
+  );
+  router.post("/changeInfor", userController.changeInfor);
+  router.post("/forgetPassword");
+  router.post("/resetPassword");
 
   return router;
 };
